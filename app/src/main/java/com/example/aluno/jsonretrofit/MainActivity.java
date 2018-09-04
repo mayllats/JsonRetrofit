9c9290c762c2041ad6dd0832a5608e33b00b86af
Olivins <diogo.olivins@gmail.com>
2018-09-03 21:22:03 -0300

    finalização trabalho

--- app/src/main/java/com/example/aluno/jsonretrofit/MainActivity.java
+++ app/src/main/java/com/example/aluno/jsonretrofit/MainActivity.java
@@ -18,6 +18,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+//
 
 public class MainActivity extends AppCompatActivity {
 
